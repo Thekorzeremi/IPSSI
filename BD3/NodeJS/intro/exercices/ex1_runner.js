fe55229f5b21b5e0dd419ecf7964a607511885c3
Remi k <remi.k@cashflowpositif.fr>
2025-03-31 11:36:30 +0200

    ci: BD3 - NodeJS - ex1

--- BD3/NodeJS/intro/exercices/ex1_runner.js
+++ BD3/NodeJS/intro/exercices/ex1_runner.js
@@ -2,7 +2,17 @@ import { sumArray } from './ex1_modules.js';
 
 let TestArrayForSumArrayModule = [69, 78, 23, 45];
 
-// Testing sumArray module
 console.log(`Testing sumArray module with array: ${TestArrayForSumArrayModule}`);
-let result = sumArray(TestArrayForSumArrayModule);
-console.log(`Result: ${result}`);   
+let result;
+let expectedResult = 215; // 69 + 78 + 23 + 45 = 215
+
+try {
+    result = sumArray(TestArrayForSumArrayModule);
+    console.log("sumArray test module passed");
+    console.log("Expected: ", expectedResult);
+    console.log("Received: ", result);
+} catch (error) {
+    console.error("sumArray test module failed: ", error.message);
+    console.log("Expected: ", expectedResult);
+    console.log("Received: ", result);
+}
